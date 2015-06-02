754cab612c745f1876343e126f5a094aeb59ab4d
Judy Lin <judith.s.lin@gmail.com>
2015-06-02 16:31:38 -0600

    title of movies as a list, not linked yet

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
+var h1 = document.createElement('h1');
+h1.innerHTML = "OMDB Movies About Pizza";
+document.body.appendChild(h1);
 
+//new XHR
+//XHR opens 'get', url??
+//xhr. when you load...(addeventlistener)
+//xhr.send
+
+var xhr = new XMLHttpRequest();
+xhr.open('get', 'http://omdbapi.com/?s=pizza');
+xhr.addEventListener('load', function () {
+//.response is a string. want objects out of it by parsing
+var response = xhr.response;
+var responseData = JSON.parse(response);
+
+for (var i = 0; i < responseData.Search.length; i++) {
+
+  var a = document.createElement('a');
+  a.innerHTML = responseData.Search[i].Title;
+  a.href= '/show.html?'
+
+  // <p><a href="/show.html?">Mystic Pizza</a></p>
+
+  var p = document.createElement('p');
+  p.appendChild(a);
+  document.body.appendChild(p);
+  }
+
+});
+xhr.send(null);
