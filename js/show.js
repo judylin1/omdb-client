f6dd7142687ba8859ec7c44f6576d9861d27e928
Judy Lin <judith.s.lin@gmail.com>
2015-06-03 21:03:37 -0600

    working, minimal styling

--- js/show.js
+++ js/show.js
@@ -11,5 +11,20 @@ var x = 'http://www.omdbapi.com/' + document.location.search + "&plot=short&r=js
     var year = document.getElementById('year');
     year.innerHTML = responseData.Year;
 
+    var rating = document.getElementById('rating');
+    rating.innerHTML = responseData.Rated;
+
+    var release = document.getElementById('release');
+    release.innerHTML = responseData.Released;
+
+    var genre = document.getElementById('genre');
+    genre.innerHTML = responseData.Genre;
+
+    var director = document.getElementById('director');
+    director.innerHTML = responseData.Director;
+
+    var imdbrating = document.getElementById('imdbrating');
+    imdbrating.innerHTML = responseData.imdbRating;s
+
   });
 xhr.send();
